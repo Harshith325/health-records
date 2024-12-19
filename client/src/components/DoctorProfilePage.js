c0987b4caa1712372d22fc9aede9b17609509138
kart2004 <karthikprakash999@gmail.com>
2024-12-19 09:47:53 +0530

    new

--- client/src/components/DoctorProfilePage.js
+++ client/src/components/DoctorProfilePage.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import '../styles/ProfilePage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const DoctorProfilePage = () => {
   const location = useLocation();
   const { username, password } = location.state;
 
   return (
-    <>
-      <div className="header">
-        <h1>Doctor Profile Page</h1>
-      </div>
-      <div className="container">
-        <div className="box">
-          <h1>Doctor Profile</h1>
-          <p>Username: {username}</p>
-          <p>Password: {password}</p>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4" style={{ paddingRight: '85px' }}>
+          <div className="shadow p-3 mb-5 bg-body-tertiary rounded" style={{ paddingTop: '50px' }}>
+            <div className="card" style={{ height: '100%' }}>
+              <img
+                src="https://images.assetsdelivery.com/compings_v2/juliatim/juliatim1607/juliatim160700013.jpg"
+                className="card-img-top mx-auto d-block"
+                style={{ height: '200px', width: '200px', objectFit: 'cover' }}
+                alt="Doctor"
+              />
+              <div className="card-body">
+                <h5 className="card-title" style={{ paddingTop: '5px' }}>Doctor Information</h5>
+                <p className="card-text"></p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item"><b>Doctor ID:</b> </li>
+                <li className="list-group-item"><b>Name:</b> </li>
+                <li className="list-group-item"><b>Speciality:</b> </li>
+                <li className="list-group-item"><b>Contact Info:</b> </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8" style={{ paddingLeft: '5px' }}>
+          <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Actions</h5>
+                <hr />
+                <div className="d-grid gap-2">
+                  <button className="btn btn-primary btn-lg">Appointments</button>
+                  <button className="btn btn-primary btn-lg">Prescription</button>
+                  <button className="btn btn-primary btn-lg">Medication</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
